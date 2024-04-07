f00a35997c2e6fe9dc8aa3979a12e5da0430d32c
Alexey <107351385+S4CBS@users.noreply.github.com>
2024-04-07 21:32:29 +0300

    upd

--- info/req.py
+++ info/req.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import bs4
 def site_status():
     try:
         with open("config.cfg", 'r') as file:
@@ -9,17 +9,13 @@ def site_status():
             golden_key = golden_key.replace("golden_key=", '')
     except FileNotFoundError:
         pass
-
     url = "https://funpay.com/orders/trade"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
         "Cookie": f"golden_key={golden_key}"
     }
-
     response = requests.get(url, headers=headers)
-
     return response.status_code
-
 def get_info(language):
     try:
         with open("config.cfg", 'r') as file:
@@ -30,7 +26,6 @@ def get_info(language):
         with open("config.cfg", 'w') as file:
             file.write("golden_key="+golden_key)
             file.close()
-        
     if language != 'ru':
         url = f"https://funpay.com/{language}/orders/trade" if language else "https://funpay.com/orders/trade"
         headers = {
@@ -43,12 +38,9 @@ def get_info(language):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
             "Cookie": f"golden_key={golden_key}"
         }
-
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-
         menu_item_chat = soup.find("a", class_="menu-item-chat")
         menu_item_trade = soup.find("a", class_="menu-item-trade")
         user_link_name = soup.find("div", class_="user-link-name")
@@ -58,27 +50,23 @@ def get_info(language):
         # Достаем ссылку на профиль
         user_link_dropdown = soup.find("a", class_="user-link-dropdown", href=True)
         href = user_link_dropdown["href"]
-
+        # Валюта
+        menu_item_currencies = soup.find("a", class_="menu-item-currencies")
         if menu_item_chat:
             chat = menu_item_chat.text.strip()
-
         if menu_item_trade:
             trade = menu_item_trade.text.strip()
-        
         if user_link_name:
             name = user_link_name.text.strip()
-        
         if menu_item_langs:
             lang = menu_item_langs.text.strip()
-        
         if menu_item_balance:
             balance = menu_item_balance.text.strip()
-        
         if menu_item_orders:
             orders = menu_item_orders.text.strip()
-    
-    return chat, trade, name, lang, balance, orders, href
-
+        if menu_item_currencies:
+            valuta = menu_item_currencies.text.strip()
+    return chat, trade, name, lang, balance, orders, href, valuta
 def get_href_url():
     try:
         href_url = get_info("ru")
@@ -90,9 +78,7 @@ def get_href_url():
         except:
             href_url = get_info("uk")
             href_url = [x for x in href_url if "https" in x]
-    
     return href_url[0]
-
 def get_rating(href=get_href_url()):
     try:
         with open("config.cfg", 'r') as file:
@@ -103,31 +89,53 @@ def get_rating(href=get_href_url()):
         with open("config.cfg", 'w') as file:
             file.write("golden_key="+golden_key)
             file.close()
-
     url = href
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
         "Cookie": f"golden_key={golden_key}"
     }
-
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
-
         big = soup.find("span", class_="big")
         text_nowrap = soup.find("div", class_="text-nowrap")
         rating_full_count = soup.find("div", class_="rating-full-count")
         # tc_item = soup.find("div", class_="tc-desc-text") Предметы на продаже
-    
         if big:
             rating = big.text.strip()
-
         if text_nowrap:
             reg_date = text_nowrap.text.strip().replace("  ", "")
-
         if rating_full_count:
             rating_full_count = rating_full_count.text.split()
             rating_full_count = [x for x in rating_full_count if x.isdigit()][0]
-        
     return rating, reg_date, rating_full_count
+def getAvatar(href=get_href_url()):
+    try:
+        with open("config.cfg", 'r') as file:
+            golden_key = file.read()
+            golden_key = golden_key.replace("golden_key=", '')
+    except FileNotFoundError:
+        golden_key = input("Введите ваш golden_key: ")
+        with open("config.cfg", 'w') as file:
+            file.write("golden_key="+golden_key)
+            file.close()
+    url = href
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+        "Cookie": f"golden_key={golden_key}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        avatar_photo = soup.find("div", class_="avatar-photo")
+        if avatar_photo:
+            # Получаем значение атрибута style
+            style = avatar_photo.get("style")
+            if style:
+                # Используем регулярное выражение для извлечения URL из атрибута style
+                pattern = r"url\((.*?)\)"
+                matches = re.search(pattern, style)
+                if matches:
+                    # Извлекаем ссылку из найденного совпадения
+                    avatar_url = matches.group(1)
+    return avatar_url
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from info.req import get_info, site_status, get_rating
+from info.req import get_info, site_status, get_rating, getAvatar
 import telebot
-
+import os
+import schedule
+import threading, time
+counter = 0
+os.system("cls")
 try:
     with open("config_tg.cfg", 'r') as file:
         tk = file.read()
@@ -10,49 +14,65 @@ except FileNotFoundError:
     with open("config_tg.cfg", 'w') as file:
         file.write("token="+tk)
         file.close()
-
-
 bot = telebot.TeleBot(token=tk)
-
 start_message = """Доступные команды:
 /status - Статус сайта
 /info - Полная информация
 ..."""
-
 def fetch_info():
     try:
-        chat, trade, name, lang, balance, orders, href = get_info("ru")
+        chat, trade, name, lang, balance, orders, href, valuta = get_info("ru")
     except:
         try:
-            chat, trade, name, lang, balance, orders, href = get_info("en")
+            chat, trade, name, lang, balance, orders, href, valuta = get_info("en")
         except:
-            chat, trade, name, lang, balance, orders, href = get_info("uk")
-
+            chat, trade, name, lang, balance, orders, href, valuta = get_info("uk")
     rating, reg_date, rating_full_count = get_rating()
-
+    avatar = getAvatar()
     edit_chat = ''.join([x for x in chat if x.isdigit()])
     edit_trades = ''.join([x for x in trade if x.isdigit()])
     edit_balance = ''.join([x for x in balance if x.isdigit()])
     edit_orders = ''.join([x for x in orders if x.isdigit()])
-
-    return edit_chat, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count
-
+    return edit_chat, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count, valuta, avatar
 @bot.message_handler(commands=['status'])
 def check_status(message):
     status = site_status()
     bot.send_message(message.chat.id, status)
-
 @bot.message_handler(commands=['start'])
 def start_cm(message):
     bot.send_message(message.chat.id, start_message)
-
 @bot.message_handler(commands=['info'])
 def info_cm(message):
-    edit_chat, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count = fetch_info()
-    bot.send_message(message.chat.id, f"Имя пользователя: {name}\nСсылка на профиль: {href}\nПродажи: {edit_trades}\nСообщения: {edit_chat}\nЯзык: {lang}\nФинансы: {edit_balance}\nПокупки: {edit_orders}\nРейтинг: {rating}\nДата регистрации: \n{reg_date}\nКоличество отзывов: {rating_full_count}")
+    edit_chat, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count, valuta, avatar = fetch_info()
+    bot.send_message(message.chat.id, f"Имя пользователя: {name}\nСсылка на профиль: {href}\nАватар: {avatar}\nПродажи: {edit_trades}\nСообщения: {edit_chat}\nЯзык: {lang}\nФинансы: {edit_balance} - Валюта {valuta}\nПокупки: {edit_orders}\nРейтинг: {rating}\nДата регистрации: \n{reg_date}\nКоличество отзывов: {rating_full_count}\n")
+#Проверка на новые сообщения
+def newMes():
+    global counter
+    edit_chat, *_ = fetch_info()
+    edit_chat = int(edit_chat)
 
-bot.polling(True)
-
-
-
+    if edit_chat > counter: # 1 0
+        bot.send_message(chat_id="YOUR CHATID", text="У вас новое сообщение!")
+        counter = edit_chat # 1 1
+    else:
+        counter = edit_chat
     
+
+def polling_thread():
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(f"Ошибка в работе бота: {e}")
+            time.sleep(10)
+# Запуск бота в отдельном потоке
+bot_thread = threading.Thread(target=polling_thread)
+bot_thread.daemon = True
+bot_thread.start()
+# Запуск планировщика для выполнения функции newMes каждую минуту
+schedule.every(15).seconds.do(newMes)
+print("Бот запущен!")
+# Бесконечный цикл для выполнения планировщика
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Ждем 1 секунду между проверками планировщика
