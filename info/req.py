3807f078005fc2d4d02eddbe9045871a54a4bacd
Alexey <107351385+S4CBS@users.noreply.github.com>
2024-04-11 22:56:35 +0300

    upd

---
+++ info/req.py
@@ -0,0 +1,177 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+def get_start_info():
+    try:
+        with open("config.cfg", "r", encoding="utf-8") as f:
+            temp = f.readlines()
+            try:
+                temp_1=temp[0].split("=")
+                tk = temp_1[1].replace("\n", "")
+            except:
+                pass
+            try:
+                temp_2 = temp[1].split("=")
+                chat_id = temp_2[1].replace("\n", "")
+            except:
+                pass
+            try:
+                temp_3 = temp[2].split("=")
+                golden_key = temp_3[1].replace("\n", "")
+            except:
+                pass
+    except FileNotFoundError as e:
+        tk = input("Введите ваш токен телеграм бота: ")
+        chat_id = input("Введите ваш chat_id телеграмм: ")
+        golden_key = input("Введите ваш golden_key с сайта funpay.com: ")
+        with open("config.cfg", "w", encoding="utf-8") as f:
+            f.write(f"""token={tk}\nchat_id={chat_id}\ngolden_key={golden_key}""")
+    return tk, chat_id, golden_key
+*_, golden_key = get_start_info()
+def site_status():
+    url = "https://funpay.com/orders/trade"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+        "Cookie": f"golden_key={golden_key}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.status_code
+def get_info(language):
+    if language != 'ru':
+        url = f"https://funpay.com/{language}/orders/trade" if language else "https://funpay.com/orders/trade"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+            "Cookie": f"golden_key={golden_key}"
+        }
+    else:
+        url = "https://funpay.com/orders/"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+            "Cookie": f"golden_key={golden_key}"
+        }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        menu_item_chat = soup.find("a", class_="menu-item-chat")
+        menu_item_trade = soup.find("a", class_="menu-item-trade")
+        user_link_name = soup.find("div", class_="user-link-name")
+        menu_item_langs = soup.find("a", class_="menu-item-langs")
+        menu_item_balance = soup.find("a", class_="menu-item-balance")
+        menu_item_orders = soup.find("a", class_="menu-item-orders")
+        # Достаем ссылку на профиль
+        user_link_dropdown = soup.find("a", class_="user-link-dropdown", href=True)
+        href = user_link_dropdown["href"]
+        # Валюта
+        menu_item_currencies = soup.find("a", class_="menu-item-currencies")
+        if menu_item_chat:
+            chat = menu_item_chat.text.strip()
+        if menu_item_trade:
+            trade = menu_item_trade.text.strip()
+        if user_link_name:
+            name = user_link_name.text.strip()
+        if menu_item_langs:
+            lang = menu_item_langs.text.strip()
+        if menu_item_balance:
+            balance = menu_item_balance.text.strip()
+        if menu_item_orders:
+            orders = menu_item_orders.text.strip()
+        if menu_item_currencies:
+            valuta = menu_item_currencies.text.strip()
+    return chat, trade, name, lang, balance, orders, href, valuta
+def get_href_url():
+    try:
+        href_url = get_info("ru")
+        href_url = [x for x in href_url if "https" in x]
+    except:
+        try:
+            href_url = get_info("en")
+            href_url = [x for x in href_url if "https" in x]
+        except:
+            href_url = get_info("uk")
+            href_url = [x for x in href_url if "https" in x]
+    return href_url[0]
+def get_rating(href=get_href_url()):
+    url = href
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+        "Cookie": f"golden_key={golden_key}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        big = soup.find("span", class_="big")
+        text_nowrap = soup.find("div", class_="text-nowrap")
+        rating_full_count = soup.find("div", class_="rating-full-count")
+        # tc_item = soup.find("div", class_="tc-desc-text") Предметы на продаже
+        if big:
+            rating = big.text.strip()
+        if text_nowrap:
+            reg_date = text_nowrap.text.strip().replace("  ", "")
+        if rating_full_count:
+            rating_full_count = rating_full_count.text.split()
+            rating_full_count = [x for x in rating_full_count if x.isdigit()][0]
+    return rating, reg_date, rating_full_count
+def getAvatar(href=get_href_url()):
+    url = href
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+        "Cookie": f"golden_key={golden_key}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        avatar_photo = soup.find("div", class_="avatar-photo")
+        if avatar_photo:
+            # Получаем значение атрибута style
+            style = avatar_photo.get("style")
+            if style:
+                # Используем регулярное выражение для извлечения URL из атрибута style
+                pattern = r"url\((.*?)\)"
+                matches = re.search(pattern, style)
+                if matches:
+                    # Извлекаем ссылку из найденного совпадения
+                    avatar_url = matches.group(1)
+    return avatar_url
+def chat_url(language):
+    if language == "ru":
+        url = "https://funpay.com/chat/"
+    elif language == "en":
+        url = "https://funpay.com/en/chat/"
+    elif language == "uk":
+        url = "https://funpay.com/uk/chat/"
+    return url
+def chat_new_notif():
+    results = []
+    try:
+        url = chat_url("ru")
+    except:
+        try:
+            url = chat_url("en")
+        except:
+            url = chat_url("uk")
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+            "Cookie": f"golden_key={golden_key}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        unread = soup.find_all("a", class_="unread")
+
+        for item in unread:
+            user_name_elem = item.find(class_="media-user-name")
+            message_elem = item.find(class_="contact-item-message")
+            time_elem = item.find(class_="contact-item-time")
+            data_node_msg = item.get("data-node-msg", "")
+        
+            if user_name_elem and message_elem and time_elem:
+                user_name = user_name_elem.get_text(strip=True)
+                message = message_elem.get_text(strip=True)
+                time = time_elem.get_text(strip=True)
+                
+                results.append((user_name,message,time, data_node_msg))
+
+        if unread != []:
+            return len(unread), results
+        else:
+            return 0, results
---
+++ main.py
@@ -0,0 +1,76 @@
+from info.req import get_info, site_status, get_rating, getAvatar, chat_new_notif, get_start_info
+import telebot
+import os
+import schedule
+import threading, time
+tk, chat_id, _ = get_start_info()
+mess_check = set()
+os.system("cls")
+bot = telebot.TeleBot(token=tk)
+start_message = """Доступные команды:
+/status - Статус сайта
+/info - Полная информация
+..."""
+def fetch_info():
+    try:
+        chat, trade, name, lang, balance, orders, href, valuta = get_info("ru")
+    except:
+        try:
+            chat, trade, name, lang, balance, orders, href, valuta = get_info("en")
+        except:
+            chat, trade, name, lang, balance, orders, href, valuta = get_info("uk")
+    rating, reg_date, rating_full_count = get_rating()
+    avatar = getAvatar()
+    mess, results = chat_new_notif()
+    edit_chat = ''.join([x for x in chat if x.isdigit()])
+    edit_trades = ''.join([x for x in trade if x.isdigit()])
+    edit_balance = ''.join([x for x in balance if x.isdigit()])
+    edit_orders = ''.join([x for x in orders if x.isdigit()])
+    try:
+        if edit_balance == "":
+            edit_balance = 0
+        else:
+            pass
+    except:
+        pass
+    return edit_chat, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count, valuta, avatar, mess, results
+@bot.message_handler(commands=['status'])
+def check_status(message):
+    status = site_status()
+    bot.send_message(message.chat.id, status)
+@bot.message_handler(commands=['start'])
+def start_cm(message):
+    bot.send_message(message.chat.id, start_message)
+@bot.message_handler(commands=['info'])
+def info_cm(message):
+    _, edit_trades, name, lang, edit_balance, edit_orders, href, rating, reg_date, rating_full_count, valuta, avatar, mess, _ = fetch_info()
+    bot.send_message(message.chat.id, f"Имя пользователя: {name}\nСсылка на профиль: {href}\nАватар: {avatar}\nПродажи: {edit_trades}\nСообщения: {mess}\nЯзык: {lang}\nФинансы: {edit_balance} - Валюта {valuta}\nПокупки: {edit_orders}\nРейтинг: {rating}\nДата регистрации: \n{reg_date}\nКоличество отзывов: {rating_full_count}\n")
+#Проверка на новые сообщения
+def newMes():
+    global mess_check
+    *_, mess, res = fetch_info()
+    for user_name,message,time, data_node_msg in res:
+        if data_node_msg not in mess_check:
+            try:
+                bot.send_message(chat_id=chat_id, text=f"У вас новое сообщение!\nUsername: {user_name}\nMessage: {message}\nTime: {time}")
+                mess_check.add(data_node_msg)
+            except Exception as e:
+                print(e)
+def polling_thread():
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(f"Ошибка в работе бота: {e}")
+            time.sleep(10)
+# Запуск бота в отдельном потоке
+bot_thread = threading.Thread(target=polling_thread)
+bot_thread.daemon = True
+bot_thread.start()
+# Запуск планировщика для выполнения функции newMes каждую минуту
+schedule.every(30).seconds.do(newMes)
+print("Бот запущен!")
+# Бесконечный цикл для выполнения планировщика
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Ждем 1 секунду между проверками планировщика
